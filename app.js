bcdf2eea4afc716ee9185397f09300f2caf6678b
plasmaster09 <spencerschrager@mac.com>
2023-05-27 04:27:28 +0000

    checkpoint! woohoo

---
+++ app.js
@@ -0,0 +1,34 @@
+//set up the server
+const express = require( "express" );
+const app = express();
+const port = 3000;
+
+// define a route for the default home page
+app.get( "/", ( req, res ) => {
+    res.sendFile( __dirname + "/views/index.html" );
+} );
+
+// define a route for the assignment list page
+app.get( "/data", ( req, res ) => {
+    res.sendFile( __dirname + "/views/data.html" );
+} );
+
+// define a route for the assignment list page
+app.get( "/categories", ( req, res ) => {
+    res.sendFile( __dirname + "/views/categories.html" );
+} );
+
+// define a route for the assignment detail page
+app.get( "/data/detail", ( req, res ) => {
+    res.sendFile( __dirname + "/views/detail.html" );
+} );
+
+// define a route for the assignment detail page
+app.get( "/categories/editor", ( req, res ) => {
+    res.sendFile( __dirname + "/views/editor.html" );
+} );
+
+// start the server
+app.listen( port, () => {
+    console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
+} );
